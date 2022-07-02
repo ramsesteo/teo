9a8bf77b6985e5dc095d2efd46749627f8d48e5e
ramsesteo <108594637+ramsesteo@users.noreply.github.com>
2022-07-03 02:48:09 +0300

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,7 +15,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://teo:<teo>@teo.xbfdndz.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://melih:melih@melih.dla4dkq.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
